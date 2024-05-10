98507f08aad5973409c6da1329a9f2d5dc7a1f9a
INDERJEETBHADU <bishnoiinderjeet580@gmail.com>
2024-05-10 14:45:49 +0530

    updates-done

--- src/components/Myths.jsx
+++ src/components/Myths.jsx
@@ -6,7 +6,7 @@ function Myths() {
     <>
       <div className=" bg_knowledege_img">
         <div className="container-2   pb-md-3">
-          <div className="row py-5">
+          <div className="row py-md-5 py-3">
             <div className="col-md-8 d-flex align-items-center">
               <div className="">
                 <h4 className=" ff-euro  fw-bold text-30 lh-49  text-black mb-0 text-capitalize">
